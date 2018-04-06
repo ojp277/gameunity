27a2751f87d6f93cab43353fb3e05d86ef62d7ef
Otto Plasmeijer <ottojohan.plasmeijer@coderclass.nl>
2018-04-06 19:34:55 +0200

    Bijna af, heb nog een paar issues die ik aan het fixen ben.

---
+++ Assets/counter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class counter : MonoBehaviour
+{
+
+    Text text;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        text.text = Counter.count.ToString();
+    }
+}
+
+public static class Counter
+{
+    public static int count;
+}
---
+++ Assets/script1.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class script1 : MonoBehaviour {
+    public int coinPickups = 0;
+    public Text coinCounter;
+    GameObject Camera;
+    Animator anim;
+    Rigidbody rigid;
+
+    // Use this for initialization
+    void Start () {
+        UpdateCounter();
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+        anim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void UpdateCounter()
+    {
+        coinCounter.text = coinPickups.ToString();
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "coin")
+        {
+            Counter.count += 1;
+            //UpdateCounter();
+        }
+        
+        //UpdateCounter();
+    }
+}
